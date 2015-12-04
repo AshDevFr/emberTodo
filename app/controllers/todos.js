9a98b8cbcd321be684ab32730cd4bba7e8069236
Ash <ashdevfr@gmail.com>
2015-12-04 15:10:02 +0100

    Add clearCompleted

--- app/controllers/todos.js
+++ app/controllers/todos.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   actions   : {
-    create : function() {
+    create         : function() {
       var title, todo;
 
       title = this.get('newTitle').trim();
@@ -17,6 +17,10 @@ export default Ember.Controller.extend({
       todo.save();
 
       this.set('newTitle', '');
+    },
+    clearCompleted : function() {
+      var completed = this.get('done');
+      completed.invoke('destroyRecord');
     }
   },
   remaining : function() {
@@ -24,5 +28,14 @@ export default Ember.Controller.extend({
       return !todo.get('isDone');
     });
     return todos.length;
-  }.property('model.length', 'model.@each.isDone')
+  }.property('model.length', 'model.@each.isDone'),
+  done      : function() {
+    let todos = this.get('model').filter((todo) => {
+      return todo.get('isDone');
+    });
+    return todos;
+  }.property('model.length', 'model.@each.isDone'),
+  hasDone   : Ember.computed('done', function() {
+    return this.get('done').length > 0;
+  })
 });
