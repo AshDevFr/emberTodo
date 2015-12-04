031dddace333885594b886b9c63eef1a2d858de0
Ash <ashdevfr@gmail.com>
2015-12-04 11:36:11 +0100

    New helper: Pluralize

--- app/helpers/pluralize.js
+++ app/helpers/pluralize.js
@@ -3,7 +3,6 @@ import Ember from 'ember';
 export function pluralize(params) {
   var inflector = Ember.Inflector.inflector;
 
-  console.log(params);
   return params[1] === 1 ? params[0] : inflector.pluralize(params[0]);
 }
 
